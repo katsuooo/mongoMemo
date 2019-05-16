cec069f5733989b21aaa98b4972e1991a6ee9a09
katsuooo <katsuooo.g@gmail.com>
2019-05-16 13:13:11 +0900

    gitrepo file name to colors

--- public/javascripts/gitRepo.js
+++ public/javascripts/gitRepo.js
@@ -13,18 +13,42 @@ var liTag = "<li class='list-group-item'>rname</li>"
 /*
  socket get
 */
-socket.on('gitRepoFiles', function(files){
-    //console.log('socketon');
-    //console.log(files);
+socket.on('gitRepoFiles_xxx', function(files){
     var newTag = '';
     files.forEach( (file) => {
         var x = liTag;
         x = x.replace('rname', file);
         newTag += x;
     });
-    //console.log('newTag', newTag);
     $('#gitrepoList').append(newTag);
 });
+
+
+/*
+ 190516
+ li >>> span
+*/
+
+var spanTag = "<span class='badge badge-pill badge-color'>fileName</span>";
+var colors = ['primary','secondary','success','danger','warning','info','light','dark'];
+socket.on('gitRepoFiles', (files)=>{
+    var newTag = '';
+    var colorNum = 0;
+    files.forEach( (file)=>{
+        var x = spanTag;
+        x = x.replace('color',colors[colorNum++]);
+        if (colorNum === colors.length()){
+            colorNum = 0;
+        }
+        x = x.replace('fileName', file);
+    });
+    $('#gitrepoInline').append(newTag);
+});
+
+
+
+
+
 
 $(document).ready(function(){
     socket.emit('gitRepoStart');
